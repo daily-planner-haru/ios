e9efcf5976e08f2a804f380ce22d6b86829b0a54
lala-david <wnswns1946@naver.com>
2023-08-01 16:25:17 +0900

    IOS 프로젝트를 구성한다. #test upload_3

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ios</Text>
+      <Text>🦀 안녕 영진아</Text>
       <StatusBar style="auto" />
     </View>
   );
