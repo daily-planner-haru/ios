164dad218c3654243937bca8a952bfe51b124e33
lala-david <wnswns1946@naver.com>
2023-08-31 13:27:45 +0900

    IOS 로그인 화면 기능 구현 #3

---
+++ src/auth/kakao.tsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+
+
+const kakao = () => {
+    return (
+      <View>
+        <Text>kakao login</Text>
+      </View>
+    );
+  };
+
+  export default kakao;
---
+++ src/auth/naver.tsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+
+
+const naver = () => {
+    return (
+      <View>
+        <Text>naver login</Text>
+      </View>
+    );
+  };
+
+  export default naver;
