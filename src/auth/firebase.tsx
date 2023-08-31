be28c5a333325e5ebb9ed5c2c672bdbc35e28df1
lala-david <wnswns1946@naver.com>
2023-08-31 13:27:42 +0900

    IOS 로그인 화면 기능 구현 #3

---
+++ src/auth/firebase.tsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+
+
+const firebase = () => {
+    return (
+      <View>
+        <Text>firebase login</Text>
+      </View>
+    );
+  };
+
+  export default firebase;
